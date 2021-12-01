9d4b6b6f55439fe3c6bec4fea46326a8b9ee4c94
Christian Hill <xn.hill@gmail.com>
2021-12-01 14:15:27 +0100

    Now version 2.3.0

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 
 long_description = """PyValem is a Python package for parsing, validating, manipulating and interpreting the chemical formulas, quantum states and labels of atoms, ions and small molecules.
@@ -7,16 +8,25 @@ Species and states are specfied as strings using a simple and flexible syntax, a
 See https://github.com/xnx/pyvalem for documentation and more information.
 """
 
+# Read in dependencies list from requirements.txt
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name = 'pyvalem',
-    version = '2.2.0',
-    author = 'Christian Hill',
+    version = '2.3.0',
+    author = 'Christian Hill, Martin Hanicinec',
     author_email = 'ch.hill@iaea.org',
     description = 'A package for managing simple chemical species and states',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url = 'https://github.com/xnx/pyvalem',
     packages = find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
