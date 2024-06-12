dbfa0ee24805a9aa323765eb24c0f1846d4cf7b3
Christian Hill <xn.hill@gmail.com>
2024-06-12 10:50:08 +0200

    Add unittest to ensure state classes are imported by states.__init__.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,9 +1,15 @@
 import unittest
 
 from pyvalem import states
+from pyvalem.states._state_parser import STATES
 
 
 class StatesImportTest(unittest.TestCase):
     def test_state_types_import(self):
         self.assertIn("AtomicTermSymbol", states.__dict__)
         self.assertIn("CompoundLSCoupling", states.__dict__)
+
+class StatesInitFileTest(unittest.TestCase):
+    def test___init___imports(self):
+        for StateClass in STATES.keys():
+            self.assertIn(StateClass.__name__, states.__dict__)
